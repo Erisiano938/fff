fc7ddd4c3cf21d533331dbae2bae4a57753cf8df
Erisiano938 <Erisiano.semedo123@gmail.com>
2025-10-22 03:54:12 +0200

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,20 +1,21 @@
+'use client';
 
-import AcademyHero from './AcademyHero';
-import CourseCategories from './CourseCategories';
-import FeaturedCourses from './FeaturedCourses';
-import LearningPath from './LearningPath';
-import BlogSection from './BlogSection';
-import SubscriptionPlans from './SubscriptionPlans';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-export default function AcademyPage() {
+export default function AdminPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/admin/dashboard');
+  }, [router]);
+
   return (
-    <div className="min-h-screen bg-white">
-      <AcademyHero />
-      <CourseCategories />
-      <FeaturedCourses />
-      <LearningPath />
-      <BlogSection />
-      <SubscriptionPlans />
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <div className="animate-spin w-8 h-8 border-2 border-yellow-400 border-t-transparent rounded-full mx-auto mb-4"></div>
+        <p className="text-gray-400">Redirection vers l'administration...</p>
+      </div>
     </div>
   );
 }
